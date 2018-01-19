fcc74a981c4401ab38ec91b1968c6f9e765a9eac
Ljubomir Miodrag <91091210@st.deltion.nl>
2018-01-19 12:41:41 +0100

    kleine verandering
    kleine verandering

--- init.php
+++ init.php
@@ -1,14 +1,6 @@
 <?php
-
-require_once('css/style.css');
 $config = require'config.php';
 require'sql/connection.php';
 require'sql/querybuilder.php';
-require'modules/register.php';
-// returnt nieuwe querybuilder class en voegt aan deze de pdo connectie toe.
-return new querybuilder(
-  //roept static function van de connection class aan
-  //en heeft deze mee aand de querybuilder
-    connection::connect($config['database'])
-  );
+$register = new RegisterModules(connection::connect($config['database']));
 ?>

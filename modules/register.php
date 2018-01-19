46934710f04d172613bc314c66c4ec464d9dbb36
joannebrouwer1995 <35484961+joannebrouwer1995@users.noreply.github.com>
2018-01-19 12:45:21 +0100

    s
    s

--- modules/register.php
+++ modules/register.php
@@ -1,28 +1,19 @@
 <?php
 
-<<<<<<< HEAD
 include('../init.php');
 include('../sql/connection.php');
 
 
-=======
->>>>>>> c6b0bd3e6c56f4e60018a41e0a6f0b3eb9cfcdff
 class RegisterModules
 {
 	protected $pdo;
 
-<<<<<<< HEAD
 	// function __construct(){
 	// 	$this->init = new Init();
 	// }
 
 	public function __construct() {
 	    $pdo = new PDO('mysql:host=localhost;dbname=examen', 'root', '');
-=======
-	//constructor heeft database connectie nodig
-	 public function __construct() {
-	    $pdo = new PDO('mysql:host=localhost;dbname=examen', 'root', 'root');
->>>>>>> c6b0bd3e6c56f4e60018a41e0a6f0b3eb9cfcdff
 			$this->pdo = $pdo;
 	  }
 
@@ -42,12 +33,7 @@ class RegisterModules
 		$admin = 1;		
 		$q = "INSERT INTO accountgegevens (email, gebruikersnaam, wachtwoord, admin, status) VALUES ('. $this->email .', '. $this->gebruikersnaam .', '. $this->wachtwoord .', '. $this->admin .', '. $this->status .')";
 
-<<<<<<< HEAD
 		$stmt = $this->init->getConnection()->prepare($q);
-=======
-		$stmt = $this->pdo->prepare("INSERT INTO accountgegevens (email, gebruikersnaam, status, admin, wachtwoord) VALUES (". $this->email .", ". $this->gebruikersnaam .", ". $this->status .", ". $this->admin .", ".$this->wachtwoord .")");
-
->>>>>>> c6b0bd3e6c56f4e60018a41e0a6f0b3eb9cfcdff
 		// var_dump($this->init->getConnection()->dump_debug_info());
 
         $stmt->execute();

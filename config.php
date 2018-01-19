18d10556ed46a421d2997e57826fb981540858c2
97034440 <9703444@st.deltion.nl>
2018-01-19 09:53:20 +0100

    Revert "Merge branch 'Sprint1' of https://github.com/97034440/Examengroep2 into Sprint1"
    This reverts commit 8066ab2ffa352b609de74be324913c3be8d08234, reversing
    changes made to 612396f6377617e2c280688a446f12b7b8c3dd51.

--- config.php
+++ config.php
@@ -4,13 +4,8 @@ return [
     'database' => [
       'name' => 'examen',
       'username' => 'root',
-<<<<<<< HEAD
       'password' => 'root',
       'connection' => 'mysql:dbname=examen;host=127.0.0.1'
-=======
-      'password' => '',
-      'connection' => 'mysql:dbname=examen;host=127.0.0.1'  
->>>>>>> 2572f0fecdfd4949ddc8eda6a8baf614777ea69d
     ]
 ]
 ?>
--- modules/register.php
+++ modules/register.php
@@ -1,32 +1,15 @@
 <?php
-<<<<<<< HEAD
 require'../init.php';
 $var = connection::connect($config['database']);
 
 class RegisterModules
 {
 	private $pdo;
-=======
-
-include('../sql/connection.php');
-
-
-class RegisterModules
-{
-	private $init;
-	private $pdo;
-	private $connect;
->>>>>>> 2572f0fecdfd4949ddc8eda6a8baf614777ea69d
 
 	// function __construct(){
 	// 	$this->init = new Init();
 	// }
-<<<<<<< HEAD
 	public function __construct(PDO $pdo) {
-=======
-
-	public function __construct() {
->>>>>>> 2572f0fecdfd4949ddc8eda6a8baf614777ea69d
     	$this->pdo = $pdo;
   }
 
@@ -52,12 +35,8 @@ class RegisterModules
 		$status = 1;
 		$admin = 1;
 
-<<<<<<< HEAD
 		$stmt = $this->pdo->prepare("INSERT INTO accountgegevens (email, gebruikersnaam, status, admin, wachtwoord) VALUES (". $this->email .", ". $this->gebruikersnaam .", ". $this->status .", ". $this->admin .", ".$this->wachtwoord .")");
 
-=======
-		$stmt = $this->pdo->getConnection()->prepare("INSERT INTO accountgegevens (email, gebruikersnaam, status, admin, wachtwoord) VALUES (". $this->email .", ". $this->gebruikersnaam .", ". $this->status .", ". $this->admin .", ".$this->wachtwoord .")");
->>>>>>> 2572f0fecdfd4949ddc8eda6a8baf614777ea69d
 		// var_dump($this->init->getConnection()->dump_debug_info());
         $stmt->execute();
 

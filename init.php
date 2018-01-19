8851f4b8ebc8fb896f14464db3462d0c3a357ae7
Ljubomir Miodrag <91091210@st.deltion.nl>
2018-01-19 10:24:36 +0100

    Revert "Merge branch 'Sprint1' of https://github.com/97034440/Examengroep2 into Sprint1"
    This reverts commit 4d0c4cbb733220683430afe4c50bc62e32fe6967, reversing
    changes made to aacab1db5c190d438ccf639655ee4d6f44039f00.

--- init.php
+++ init.php
@@ -4,11 +4,11 @@ require_once('css/style.css');
 $config = require'config.php';
 require'sql/connection.php';
 require'sql/querybuilder.php';
+require'modules/register.php';
 // returnt nieuwe querybuilder class en voegt aan deze de pdo connectie toe.
 return new querybuilder(
   //roept static function van de connection class aan
   //en heeft deze mee aand de querybuilder
     connection::connect($config['database'])
   );
-
 ?>

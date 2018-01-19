c6b0bd3e6c56f4e60018a41e0a6f0b3eb9cfcdff
Ljubomir Miodrag <91091210@st.deltion.nl>
2018-01-19 12:42:46 +0100

    t
    t

--- init.php
+++ init.php
@@ -2,9 +2,9 @@
 $config = require'config.php';
 require'sql/connection.php';
 require'sql/querybuilder.php';
-<<<<<<< HEAD
+
 $register = new RegisterModules(connection::connect($config['database']));
-=======
+
 require'modules/register.php';
 // returnt nieuwe querybuilder class en voegt aan deze de pdo connectie toe.
 return new querybuilder(
@@ -12,5 +12,4 @@ return new querybuilder(
   //en heeft deze mee aand de querybuilder
     connection::connect($config['database'])
   );
->>>>>>> 2f179c30d994401db48a9afac5e8a7e1a40338f1
 ?>

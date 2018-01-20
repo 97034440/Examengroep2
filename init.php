4777c8e5744130dae51e1738f93c822372e3c97d
LjubomirMiodrag <35521361+LjubomirMiodrag@users.noreply.github.com>
2018-01-20 18:57:48 +0100

    slaat nergens op
    slaat nergens op

--- init.php
+++ init.php
@@ -2,8 +2,7 @@
 $config = require'config.php';
 require'sql/connection.php';
 require'sql/querybuilder.php';
-<<<<<<< HEAD
-=======
+
 require'modules/user.php';
 // returnt nieuwe querybuilder class en voegt aan deze de pdo connectie toe.
 return new querybuilder(
@@ -12,5 +11,5 @@ return new querybuilder(
     connection::connect($config['database'])
   );
 
->>>>>>> 233c76219f63f9a1ec3781cb1fede734cea94ffb
+
 ?>

c351873620cfcb24960afa8a249f70834d134a65
97034440 <9703444@st.deltion.nl>
2018-01-21 13:46:16 +0100

    inloggen werkt
    je kan inloggen

---
+++ functions/login.php
@@ -0,0 +1,55 @@
+<?php
+// $config = require'../config.php';
+// require'../sql/connection.php';
+//
+// $pdo = connection::connect($config['database']);
+//
+// $statement = $pdo->prepare("SELECT * FROM accountgegevens");
+// $result = $statement->execute();
+// $obj = $statement->fetch();
+// var_dump($obj);
+
+ session_start();
+ $host = "localhost";
+ $username = "root";
+ $password = "root";
+ $database = "examen";
+ $message = "";
+ try
+ {
+      $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);
+      $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if(isset($_POST["login"]))
+      {
+           if(empty($_POST["username"]) || empty($_POST["password"]))
+           {
+                $message = '<label>Alle velden zijn vereist</label>';
+           }
+           else
+           {
+                $query = "SELECT * FROM accountgegevens WHERE gebruikersnaam = :username AND wachtwoord = :password";
+                $statement = $connect->prepare($query);
+                $statement->execute(
+                     array(
+                          'username'     =>     $_POST["username"],
+                          'password'     =>     $_POST["password"]
+                     )
+                );
+                $count = $statement->rowCount();
+                if($count > 0)
+                {
+                     $_SESSION["username"] = $_POST["username"];
+                     header("location:login_success.php");
+                }
+                else
+                {
+                     $message = '<label>Verkeerde gegevens</label>';
+                }
+           }
+      }
+ }
+ catch(PDOException $error)
+ {
+      $message = $error->getMessage();
+ }
+ ?>

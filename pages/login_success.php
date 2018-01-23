30a6be2c2821c9ab4b6b951eb4dff28d35534d96
97034440 <9703444@st.deltion.nl>
2018-01-23 11:16:23 +0100

    extra menubalk met loguit en info
    extra menubalk

--- pages/login_success.php
+++ pages/login_success.php
@@ -11,3 +11,13 @@
       header("location:login.php"); // asl je niet kan inloggen ga je terug naar de login.php
  }
  ?>
+
+ <?php
+  //  session_start();
+  //    if($_SESSION['logged']==true){
+  //        echo $_SESSION["username"];
+  //        echo '<a href="logout.php"><span>Logout</span></a></li>';
+  //        }
+  //    elseif($_SESSION['logged']==false)
+  //        echo '<a href="registerform.html"><span>Login/Register</span></a></li>';
+  //    ?>

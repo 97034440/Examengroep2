bc438750459b3177ff2bd6ce27ac2f9a90964db5
Ljubomir Miodrag <91091210@st.deltion.nl>
2018-01-23 12:05:11 +0100

    veel aanpassingen
    veel aanpassingen

--- sql/querybuilder.php
+++ sql/querybuilder.php
@@ -10,16 +10,19 @@ protected $pdo;
   //ontvangt de tabelnaam en de classnaam waar je deze aan koppelt
   public function selectAll ($table){
     //selecteerd alles uit de tabelnaam die je hebt meegegeven.
-    $statement = $this->pdo->prepare("select * from {$table} ORDER BY id ASC");
+    $tableinsert = htmlspecialchars($table);
+    $statement = $this->pdo->prepare("select * from {$tableinsert} ORDER BY id ASC");
     // statement uitvoeren.
     $statement->execute(array("\xbf\x27 OR 1=1 /*"));
-
     return $statement->fetchAll(PDO::FETCH_CLASS);
     // alles uit de database lezen.
   }
   public function selectImage ($table, $row, $objectid){
+    $tableinsert = htmlspecialchars($table);
+    $rowinsert = htmlspecialchars($row);
+    $objectidinsert = htmlspecialchars($objectid);
     //selecteerd alles uit de tabelnaam die je hebt meegegeven.
-    $statement = $this->pdo->prepare("SELECT * FROM {$table} WHERE {$row} = {$objectid};");
+    $statement = $this->pdo->prepare("SELECT * FROM {$tableinsert} WHERE {$rowinsert} = {$objectidinsert};");
 
     // statement uitvoeren.
     $statement->execute(array("\xbf\x27 OR 1=1 /*"));
